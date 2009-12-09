09077dcc1ad797410e4433d33c0760873fa2d2af
Richard Barlow <richard@richardbarlow.co.uk>
2009-12-09 22:54:01 +0000

    Added Test class and ability to load trac wiki page for current test

--- testparser.py
+++ testparser.py
@@ -1,5 +1,16 @@
 import xml.etree.ElementTree as ET
 
+class Test:
+    """A class to represent an individual test"""
+    text = ""
+    name = ""
+    passed = False;
+    
+    def __init__(self, xmlnode):
+        self.text = xmlnode.text
+        self.name = xmlnode.get("name", "")
+
+
 def parse_file(fname):
     tree = ET.parse(fname)
 
@@ -8,7 +19,7 @@ def parse_file(fname):
 
     for e in root:
         if e.tag == "test":
-            tests.append( e.text )
+            tests.append( Test(e) )
 
     return tests
 
